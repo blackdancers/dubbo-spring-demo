ac299efea3f60bc68e5c35fd49691c2fa34dc5a3
liu_debin <liu_debin@finebei.com>
2019-06-24 16:39:32 +0800

    新增zipkin链路测试

--- dubbo-spring-provider/src/main/java/org/apache/dubbo/demo/main/Provider.java
+++ dubbo-spring-provider/src/main/java/org/apache/dubbo/demo/main/Provider.java
@@ -4,7 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  
 public class Provider {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"provider.xml"});
+        ClassPathXmlApplicationContext context =
+//                new ClassPathXmlApplicationContext(new String[] {"provider.xml"});
+                new ClassPathXmlApplicationContext(new String[] {"zipkin-provider.xml"});
         context.start();
         System.in.read(); // 按任意键退出
     }
